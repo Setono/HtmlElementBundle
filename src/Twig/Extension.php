683041b2a8e66937063049a8033db27ea5e84cc7
Joachim Løvgaard <joachim@loevgaard.dk>
2023-01-27 13:20:59 +0100

    Add 'add_class' filter

--- src/Twig/Extension.php
+++ src/Twig/Extension.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Setono\HtmlElementBundle\Twig;
 
+use Setono\HtmlElement\HtmlElement;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 final class Extension extends AbstractExtension
@@ -14,5 +16,17 @@ final class Extension extends AbstractExtension
         return [
             new TwigFunction('element', [Runtime::class, 'element'], ['is_safe' => ['html']]),
         ];
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('add_class', [$this, 'addClass'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function addClass(HtmlElement $htmlElement, string ...$class): HtmlElement
+    {
+        return $htmlElement->withClass(...$class);
     }
 }
